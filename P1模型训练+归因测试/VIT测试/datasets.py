f5fc74f703c224f5c945665a442a4cb236efd62f
KxPlaug <3175389813@qq.com>
2023-01-22 18:45:14 +0800

    1

---
+++ P1模型训练+归因测试/VIT测试/datasets.py
@@ -0,0 +1,29 @@
+from torchvision.datasets import CIFAR10
+from torchvision import transforms
+import numpy as np
+from torch.utils.data import DataLoader, SubsetRandomSampler
+
+
+def load_cifar10(root='./data', download=True):
+    transform = transforms.Compose([
+        transforms.Resize(384),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+    train_dataset = CIFAR10(root=root, train=True,
+                            download=download, transform=transform)
+    test_dataset = CIFAR10(root=root, train=False,
+                           download=download, transform=transform)
+    train_targets = np.array(train_dataset.targets)
+    test_targets = np.array(test_dataset.targets)
+    test_dataloader_all = DataLoader(
+        test_dataset, batch_size=256, shuffle=False)
+    num_classes = 10
+    train_dataloaders = []
+    test_dataloaders = []
+    for i in range(num_classes):
+        train_dataloaders.append(DataLoader(train_dataset, batch_size=256, shuffle=False,
+                                 sampler=SubsetRandomSampler(np.where(train_targets == i)[0])))
+        test_dataloaders.append(DataLoader(test_dataset, batch_size=256, shuffle=False,
+                                sampler=SubsetRandomSampler(np.where(test_targets == i)[0])))
+    return train_dataloaders, test_dataloaders, test_dataloader_all
